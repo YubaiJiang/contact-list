c07b00c42bd3f5d20723e44f081fb9ffaccad34e
Yubai Jiang <jiangyubai@gmail.com>
2022-04-15 11:02:54 +1000

    Updates

---
+++ precache-manifest.a98b1b42a577caa8ed09497d3d1492c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cbf57377c29e2f176da",
+    "url": "/contact-list/static/css/main.4e2477ce.chunk.css"
+  },
+  {
+    "revision": "3cbf57377c29e2f176da",
+    "url": "/contact-list/static/js/main.3cbf5737.chunk.js"
+  },
+  {
+    "revision": "97106c79ecfc85355927",
+    "url": "/contact-list/static/js/1.97106c79.chunk.js"
+  },
+  {
+    "revision": "377b31ea3711a9d6ba50",
+    "url": "/contact-list/static/js/runtime~main.377b31ea.js"
+  },
+  {
+    "revision": "20daa8327096f53250444f60497511de",
+    "url": "/contact-list/index.html"
+  }
+];
